91b5563f51da545f3bea3bb8f4439dcf77bdbee0
纵昂 <zongang@zongangdemacbook-pro.local>
2017-02-17 16:22:36 +0800

    Initial Commit

---
+++ 验证倒计时/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  验证倒计时
+//
+//  Created by 纵昂 on 2017/2/17.
+//  Copyright © 2017年 纵昂. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 验证倒计时/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  验证倒计时
+//
+//  Created by 纵昂 on 2017/2/17.
+//  Copyright © 2017年 纵昂. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
